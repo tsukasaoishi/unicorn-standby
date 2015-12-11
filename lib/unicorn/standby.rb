6101ec2a6662e3e2d29cb59def9d13e3409c9b6a
Tsukasa OISHI <tsukasa.oishi@gmail.com>
2015-12-12 06:06:52 +0900

    Refactoring

--- lib/unicorn/standby.rb
+++ lib/unicorn/standby.rb
@@ -3,6 +3,9 @@ require 'unicorn/http_server'
 
 module Unicorn
   module Standby
+    EXIT_SIGS = [:QUIT, :TERM, :INT]
+    private_constant :EXIT_SIGS
+
     def start
       @_wakeup ? super : self
     end
@@ -11,75 +14,79 @@ module Unicorn
       @_wakeup ? super : standby
     end
 
-    def standby
-      proc_name 'master (standby)'
-      ready_standby_trap
-
-      standby_listeners = wait_request_listeners
-      logger.info "standby ready"
-
-      if @ready_pipe
-        begin
-          @ready_pipe.syswrite($$.to_s)
-        rescue => e
-          logger.warn("grandparent died too soon?: #{e.message} (#{e.class})")
-        end
-        @ready_pipe = @ready_pipe.close rescue nil
-      end
-
-      IO.select(standby_listeners)
-
-      @_wakeup = true
-      turn_on(standby_listeners)
-    end
-
     private
 
+    def inherit_listeners!
+      super unless @_wakeup
+    end
+
+    def bind_new_listeners!
+      super unless @_wakeup
+    end
+
+    def standby
+      self.pid = config[:pid]
+
+      proc_name 'master (standby)'
+
+      kill_old_master
+      ready_standby_trap
+
+      inherit_listeners!
+      bind_new_listeners!
+
+      logger.info "standby ready"
+      notice_to_grandparent
+
+      IO.select(self.class.const_get(:LISTENERS))
+
+      turn_on
+    end
+
+    def kill_old_master
+      old_pid = "#{config[:pid]}.oldbin"
+      if File.exists?(old_pid) && pid != old_pid
+        sig = :QUIT
+        logger.info "Sending #{sig} signal to old unicorn master..."
+        Process.kill(sig, File.read(old_pid).to_i)
+      end
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+
     def ready_standby_trap
-      [:QUIT, :TERM, :INT].each do |signal|
+      (self.class.const_get(:QUEUE_SIGS) - EXIT_SIGS).each do |signal|
+        Signal.trap(signal) {}
+      end
+
+      EXIT_SIGS.each do |signal|
         Signal.trap(signal) { standby_shutdown }
       end
     end
 
-    def wait_request_listeners
-      listeners = ENV['UNICORN_FD'].to_s.split(/,/).map do |fd|
-        io = Socket.for_fd(fd.to_i)
-        prevent_autoclose(io)
-        logger.info "inherited addr=#{sock_name(io)} fd=#{fd}"
-        server_cast(io)
+    def notice_to_grandparent
+      return unless @ready_pipe
+
+      begin
+        @ready_pipe.syswrite($$.to_s)
+      rescue => e
+        logger.warn("grandparent died too soon?: #{e.message} (#{e.class})")
       end
-
-      names = listeners.map { |io| sock_name(io) }
-
-      new_listeners = config[:listeners].dup - names
-      if new_listeners.empty? && listeners.empty?
-        new_listeners << Unicorn::Const::DEFAULT_LISTEN
-      end
-
-      listeners + new_listeners.map {|addr| listen(addr) }
+      @ready_pipe = @ready_pipe.close rescue nil
     end
 
-    def turn_on(standby_listeners)
-      set_unicorn_fds(standby_listeners)
+    def turn_on
+      logger.info "standby master wake up..."
+      @_wakeup = true
       start.join
     end
 
     def standby_shutdown
-      logger.info "master complete (standby)"
+      unlink_pid_safe(pid) if pid
       exit 0
-    end
-
-    def set_unicorn_fds(standby_listeners)
-      listener_fds = {}
-      standby_listeners.each do |sock|
-        sock.close_on_exec = false if sock.respond_to?(:close_on_exec=)
-        listener_fds[sock.fileno] = sock
-      end
-      ENV['UNICORN_FD'] = listener_fds.keys.join(',')
     end
   end
 end
 
-unless Unicorn::HttpServer.include?(Unicorn::Standby)
+unless Unicorn::HttpServer.ancestors.include?(Unicorn::Standby)
   Unicorn::HttpServer.__send__(:prepend , Unicorn::Standby)
 end
